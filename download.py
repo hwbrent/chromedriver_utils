765796edd4e35dcc52efa03c729e298229bd6c2b
hwbrent <68196383+hwbrent@users.noreply.github.com>
2026-09-16 07:28:11 +0100

    Change default platform
    I'm on apple silicon now, so mac-x64 doesn't work

--- download.py
+++ download.py
@@ -9,7 +9,7 @@ import stat
 import requests
 
 PLATFORMS = ["linux64", "mac-arm64", "mac-x64", "win32", "win64"]
-PLATFORM = PLATFORMS[2]
+PLATFORM = PLATFORMS[1]
 
 ### Consts used in the retrieval of the Chrome version number
 CHROME_PLIST_PATH = "/Applications/Google Chrome.app/Contents/Info.plist"
